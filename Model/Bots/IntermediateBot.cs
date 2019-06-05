d13e546da7d48a2abcdcb1b874b21e0dbb8ccd78
Ivar Josefsson <ivar_busig@hotmail.com>
2019-06-05 11:26:57 +0200

    Added an IntermediateBot with a bit more logic

---
+++ Model/Bots/IntermediateBot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Simulation.Core;
+
+namespace Simulation.Bots
+{
+    class IntermediateBot : Bot
+
+    {
+        public Result Play(IBlackjack game)
+        {
+            int houseVal = game.DealerHand.Value;
+            if (houseVal <= 6 && game.PlayerHand.Value > 11)
+            {
+                return game.Stand();
+            }
+            else if (houseVal <= 7 && game.PlayerHand.Value <= 11)
+            {
+                return game.Hit();
+            }
+            else if (houseVal >= 7 && game.PlayerHand.Value <= houseVal + 10)
+            {
+                return game.Hit();
+            }
+            return game.Stand();
+        }
+    }
+}
